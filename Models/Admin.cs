6464f1a534b07f547f46cea52d8efc2a9af9f766
Greed <106820018+maxberglund2@users.noreply.github.com>
2025-10-03 18:45:01 +0200

    admin update

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -1,6 +1,19 @@
-﻿namespace BAMF_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BAMF_API.Models
 {
     public class Admin
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public required string UserName { get; set; }
+
+        [Required]
+        public required string PasswordHash { get; set; }
+
+        [Required]
+        public required string PasswordSalt { get; set; }
     }
 }
