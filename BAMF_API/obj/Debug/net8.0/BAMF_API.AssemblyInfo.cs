c4bc3d34eb9120e093716b4dbe750654fc266e7e
MyNameJaeff <fredrik.andersson@chasacademy.se>
2025-10-03 12:53:37 +0200

    Update .gitignore and add EF Core/JWT packages
    Expanded .gitignore to cover common .NET, Visual Studio, and Rider files. Added Entity Framework Core, Identity, and JWT authentication NuGet packages to BAMF_API.csproj to support database and authentication features.

--- BAMF_API/obj/Debug/net8.0/BAMF_API.AssemblyInfo.cs
+++ BAMF_API/obj/Debug/net8.0/BAMF_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BAMF_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99d9e84fd025f944974913bef3f50576f79bc440")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93e213addcc864e62a2359ebf6e6fd08e871b3e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BAMF_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BAMF_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
