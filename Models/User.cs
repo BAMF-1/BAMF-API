00cf947bbf9c50a4b54c6e5457ae7d11c744b176
Greed <106820018+maxberglund2@users.noreply.github.com>
2025-10-03 13:39:46 +0200

    trying to psuh models again
    fredrik didnt get it

--- Models/User.cs
+++ Models/User.cs
@@ -1,6 +1,30 @@
-﻿namespace BAMF_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+public class User
 {
-    public class User
-    {
-    }
+    public int Id { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public required string Email { get; set; }
+
+    [Required]
+    public required string PasswordHash { get; set; }
+
+    [Required]
+    public required string PasswordSalt { get; set; }
+
+    public ICollection<CartItem> Cart { get; init; } = new List<CartItem>();
+}
+
+public class CartItem
+{
+    public int Id { get; set; }
+    public int ProductId { get; set; }
+    public int Quantity { get; set; }
+
+    // Foreign Key
+    public int UserId { get; set; }
+
+    public required User User { get; set; }
 }
