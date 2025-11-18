89be999c866d9c437cbd982140ece7429552a68a
MyNameJaeff <fredrik.andersson@chasacademy.se>
2025-11-18 10:41:25 +0100

    Remove migration and seed endpoints, update CORS origins
    Removed temporary /admin/migrate and /admin/seed endpoints from Program.cs and updated CORS policy to include the Azure deployment URL. Also cleaned up startup comments and updated the root endpoint's listed endpoints.

--- obj/Debug/net8.0/BAMF_API.AssemblyInfo.cs
+++ obj/Debug/net8.0/BAMF_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BAMF_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2185720c436c6d975ca740596754ad3fb8195af0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dbcb0a091ae083113649a6cdea820be2e2381b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BAMF_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BAMF_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
